ca1548e35efd317b1d8e56c3906052408c304b7b
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-07-31 03:59:25 -0300

    🗃 Factory / Seeder User completion

---
+++ database/seeders/Users/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders\Users;
+
+use App\Models\Users\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->create([
+            'name'              => 'Juan M. Cortéz',
+            'email'             => 'juanm.cortez@gmail.com',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+        ]);
+    }
+}

ab8a30608805fbe6a54658b2d2a5473b3e1a1bd0
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-08-02 17:28:42 -0300

    💄 Encounter / Problem Tab Details

--- resources/js/Pages/Invoices/Encounter.vue
+++ resources/js/Pages/Invoices/Encounter.vue
@@ -37,7 +37,7 @@ const props = defineProps({
                                 <tr>
                                     <th colspan="6"
                                         class="p-4 font-semibold border border-slate-300 dark:border-slate-600 text-slate-900 dark:text-slate-200">
-                                        Encounter</th>
+                                        Encounter Info</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -91,6 +91,23 @@ const props = defineProps({
                                     <td
                                         class="p-4 border border-slate-300 dark:border-slate-700 text-slate-500 dark:text-slate-400">
                                         {{ invoice.sensitivity }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table
+                            class="w-full mb-10 text-sm bg-white border border-collapse shadow-sm border-slate-400 dark:border-slate-500 dark:bg-slate-800">
+                            <thead class="bg-slate-50 dark:bg-slate-700">
+                                <tr>
+                                    <th
+                                        class="p-4 font-semibold text-center border border-slate-300 dark:border-slate-600 text-slate-900 dark:text-slate-200">
+                                        Encounter Tabs</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td
+                                        class="p-4 border border-slate-300 dark:border-slate-700 text-slate-500 dark:text-slate-400">
+                                        {{ invoice.problem_tab }}</td>
                                 </tr>
                             </tbody>
                         </table>

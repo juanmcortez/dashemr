07554452b731e424f9d621ba5c3d3168f15fdd96
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-08-03 09:20:53 -0300

    🚧 Charge Model / Controller / etc,

---
+++ database/migrations/2022_08_03_114431_create_charges_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charges', function (Blueprint $table) {
+            $table->id('charge');
+
+            $table->unsignedBigInteger('encounter');
+            $table->foreign('encounter')->references('encounter')->on('encounters');
+
+            $table->enum('codeType', ['CPT4', 'HCPCS', 'ANES', 'CVX', ''])->default('CPT4')->nullable();
+            $table->string('code', 8)->default('99213');
+            $table->string('codeText', 128)->nullable();
+
+            $table->float('fee', 8, 2)->default(0);
+            $table->float('copay', 8, 2)->default(0);
+            $table->integer('units')->default(1);
+
+            $table->string('modifier', 64)->nullable();
+            $table->text('ICDitems')->nullable();
+
+            $table->string('NDCvalue', 32)->nullable();
+            $table->integer('NDCquantity')->nullable();
+            $table->enum('NDCtype', ['ML', 'Grams', 'Miligrams', 'I.U.', 'Units'])->default('ML');
+
+            $table->text('noteCodes')->nullable();
+
+            $table->string('custom1', 64)->nullable();
+            $table->string('custom2', 64)->nullable();
+            $table->string('custom3', 64)->nullable();
+            $table->string('custom4', 64)->nullable();
+            $table->string('custom5', 64)->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('charges');
+    }
+};

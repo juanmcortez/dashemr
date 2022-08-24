94cd6faa41ada3638ddd1a0284f07fe8b2f3c6d2
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-08-24 08:19:27 -0300

    🚧 Practice Settings Model / Controller / etc.

---
+++ app/Models/Settings/Practice.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models\Settings;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Practice extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'isLabPractice',
+        'labPracticeType',
+    ];
+
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'isLabPractice'     => 'boolean',
+        'labPracticeType'   => 'boolean',
+    ];
+}

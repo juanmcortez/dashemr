6f442b455b192ed07f5d8fdf3df4c9758d088364
Juan M. Cortéz <juanm.cortez@gmail.com>
2022-09-06 17:54:54 -0300

    🚧 Correction on class name

--- app/Http/Requests/Users/UpdateUserRequest.php
+++ app/Http/Requests/Users/UpdateUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUsersRequest extends FormRequest
+class UpdateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
